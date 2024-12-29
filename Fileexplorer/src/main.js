7f0540d7f3c0bb88b61f3264103f38414c5a612a
Sakulboss <150686334+Sakulboss@users.noreply.github.com>
2024-12-29 19:26:12 +0100

    Cleaned up main.js.

--- Fileexplorer/src/main.js
+++ Fileexplorer/src/main.js
@@ -3,42 +3,6 @@ const { invoke } = window.__TAURI__.core;
 let filePathInputEl;
 let resultText;
 
-/*document.getElementById('file-path-selector').addEventListener('click', async () => {
-  const filepath = document.getElementById('file-path').value;
-  const fileListElement = document.getElementById('fileList');
-  fileListElement.innerHTML = ''; // Clear previous results
-
-  try {
-    const entries = await invoke('format_file_data', { path: filepath });
-    entries.forEach(entry => {
-      const row = document.createElement('tr');
-      const filenameCell = document.createElement('td');
-      const lastModifiedCell = document.createElement('td');
-      const fileTypeCell = document.createElement('td');
-      const fileSizeCell = document.createElement('td');
-
-      filenameCell.textContent = entry.name; // Display the filename
-      lastModifiedCell.textContent = entry.last_modified; // Display last modified date
-      fileTypeCell.textContent = entry.file_type; // Display file type
-      fileSizeCell.textContent = entry.size
-
-      row.appendChild(filenameCell);
-      row.appendChild(lastModifiedCell);
-      row.appendChild(fileTypeCell);
-      row.appendChild(fileSizeCell);
-      fileListElement.appendChild(row);
-    });
-  } catch (error) {
-    console.error('Error:', error);
-    const row = document.createElement('tr');
-    const errorCell = document.createElement('td');
-    errorCell.colSpan = 3; // Span across all columns
-    errorCell.textContent = 'Error: ' + error; // Display the error message
-    errorCell.classList.add('error'); // Add error class for styling
-    row.appendChild(errorCell);
-    fileListElement.appendChild(row);
-  }
-});*/
 async function loadFilesAndFolders() {
   const filepath = document.getElementById('file-path').value; // Aktuellen Pfad auslesen
   const fileListElement = document.getElementById('fileList');
@@ -77,16 +41,6 @@ async function loadFilesAndFolders() {
     errorMessageElement.textContent = 'Error: ' + error; // Fehlermeldung setzen
     errorMessageElement.classList.remove('hidden'); // Meldung sichtbar machen
   }
-  /*} catch (error) {
-    console.error('Error:', error);
-    const row = document.createElement('tr');
-    const errorCell = document.createElement('td');
-    errorCell.colSpan = 3; // Über alle Spalten spannen
-    errorCell.textContent = 'Error: ' + error; // Fehlermeldung anzeigen
-    errorCell.classList.add('error'); // Fehlerklasse hinzufügen
-    row.appendChild(errorCell);
-    fileListElement.appendChild(row);
-  } */
 }
 
 
