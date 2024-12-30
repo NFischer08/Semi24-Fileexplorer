483e7b5c412197621ffaa90ff3847ca46e537a77
Sakulboss <150686334+Sakulboss@users.noreply.github.com>
2024-12-30 11:09:32 +0100

    Added settings button

--- Fileexplorer/src/main.js
+++ Fileexplorer/src/main.js
@@ -11,10 +11,8 @@ async function loadFilesAndFolders() {
   errorMessageElement.classList.add('hidden');
 
   try {
-    // Dateien und Ordner abrufen
     const entries = await invoke('format_file_data', { path: filepath });
 
-    // Ergebnisse durchlaufen und in die Tabelle einfügen
     entries.forEach(entry => {
       const row = document.createElement('tr');
       const filenameCell = document.createElement('td');
@@ -43,6 +41,47 @@ async function loadFilesAndFolders() {
   }
 }
 
+async function loadSearchResults() {
+  //Initialisierung
+  const filepath = document.getElementById('file-path').value; // Aktuellen Pfad auslesen
+  const fileListElement = document.getElementById('fileList');
+  const errorMessageElement = document.getElementById('error-message');
+  fileListElement.innerHTML = ''; // Vorherige Ergebnisse löschen
+  errorMessageElement.classList.add('hidden');
+
+  try {
+    const entries = await invoke('search_results', { path: filepath });
+
+    entries.forEach(entry => {
+      const row = document.createElement('tr');
+      const filenameCell = document.createElement('td');
+      const lastModifiedCell = document.createElement('td');
+      const fileTypeCell = document.createElement('td');
+      const fileSizeCell = document.createElement('td');
+      const pathCell = document.createElement('tr');
+
+      filenameCell.textContent = entry.name; // Dateiname
+      lastModifiedCell.textContent = entry.last_modified; // Letzte Änderung
+      fileTypeCell.textContent = entry.file_type; // Dateityp
+      fileSizeCell.textContent = entry.size; //Größe
+      pathCell.textContent = entry.path; // Path
+
+      row.appendChild(filenameCell);
+      row.appendChild(lastModifiedCell);
+      row.appendChild(fileTypeCell);
+      row.appendChild(fileSizeCell);
+      fileListElement.appendChild(row);
+      fileListElement.appendChild(pathCell);
+    });
+
+  } catch (error) {
+    console.error('Error:', error);
+
+    // Fehlermeldung unter der Tabelle anzeigen
+    errorMessageElement.textContent = 'Error: ' + error; // Fehlermeldung setzen
+    errorMessageElement.classList.remove('hidden'); // Meldung sichtbar machen
+  }
+}
 
 document.getElementById('file-path-selector').addEventListener('click', async () => {
   await loadFilesAndFolders();
@@ -52,6 +91,25 @@ document.addEventListener('DOMContentLoaded', async () => {
   await loadFilesAndFolders();
 });
 
+//Einstellungskästchen
+const settingsButton = document.getElementById('settings-button');
+const settingsModal = document.getElementById('settings-modal');
+const closeModal = document.getElementById('close-modal');
+
+
+settingsButton.addEventListener('click', () => {
+  settingsModal.classList.remove('hidden');
+});
+
+closeModal.addEventListener('click', () => {
+  settingsModal.classList.add('hidden');
+});
+
+window.addEventListener('click', (event) => {
+  if (event.target === settingsModal) {
+    settingsModal.classList.add('hidden');
+  }
+});
 
 window.addEventListener("DOMContentLoaded", () => {
   filePathInputEl = document.querySelector("#file-path");
