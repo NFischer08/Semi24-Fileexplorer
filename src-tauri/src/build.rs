0c511bba3f9c9dfe2d55df765be6f9dc95cab075
Magnus <Magnus.S@eclipso.de>
2024-12-30 13:47:59 +0100

    Should be fixed, further testing needed

--- src-tauri/src/build.rs
+++ src-tauri/src/build.rs
@@ -1,8 +1,11 @@
+use embed_manifest::embed_manifest_file;
+
+
 fn main() {
     println!("cargo:rerun-if-env-changed=TAURI_CONF");
 
     // Check if the TAURI_CONF environment variable is set
-    match std::env::var("/home/magnus/RustroverProjects/Semi24-Fileexplorer/Fileexplorer/src-tauri/tauri.conf.json") {
+    match std::env::var("tauri.conf.json") {
         Ok(config_path) => {
             println!("cargo:warning=Using Tauri config at: {}", config_path);
         }
@@ -10,4 +13,7 @@ fn main() {
             println!("cargo:warning=No TAURI_CONF environment variable set, using default location.");
         }
     }
+    embed_manifest_file("build.exe.manifest")
+        .expect("unable to embed manifest file");
+    println!("cargo:rerun-if-changed=sample.exe.manifest");
 }
