e747681e7fba5d2a81a390dec859853047f1b7b7
magnus <Magnus.s@eclipso.de>
2025-01-20 20:01:38 +0100

    Idk what is happening but postgreql was a pain to set up I will check later if anything is working correctly

--- src-tauri/src/creating_database.rs
+++ src-tauri/src/creating_database.rs
@@ -1,39 +1,63 @@
-use serde::{Deserialize, Serialize};
-use surrealdb::engine::any::connect;
-use surrealdb::opt::auth::Root;
-use surrealdb::RecordId;
+#![allow(unused)] // silence unused warnings while exploring (to comment out)
 
-#[derive(Debug, Serialize, Deserialize)]
-struct Person {
-    name: Option<String>,
-    marketing: Option<bool>
+use sqlx::postgres::{PgPoolOptions, PgRow};
+use sqlx::{FromRow, Row};
+
+#[derive(Debug, FromRow)]
+struct Ticket {
+    id: i64,
+    name: String,
 }
 
-#[derive(Debug, Deserialize)]
-struct Record {
-    id: RecordId,
-}
 #[tokio::main]
-async fn main() -> surrealdb::Result<()> {
-    let db = connect("ws://localhost:8000").await?;
-    db.signin(Root {
-        username: "root",
-        password: "root",
-    })
+async fn main() -> Result<(), sqlx::Error> {
+    // 1) Create a connection pool
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres://postgres:welcome@localhost/postgres")
         .await?;
-    db.use_ns("ns").use_db("db").await?;
 
-    // Create a record with a random ID
-    let person: Option<Person> = db.create("person").await?;
-    dbg!(person);
-    // Create a record with a specific ID
-    let record: Option<Record> = db
-        .create(("person", "tobie"))
-        .content(Person {
-            name: Some("Tobie".into()),
-            marketing: Some(true),
-        })
+    // 2) Create table if not exist yet
+    sqlx::query(
+        r#"
+CREATE TABLE IF NOT EXISTS ticket (
+  id bigserial,
+  name text
+);"#,
+    )
+        .execute(&pool)
         .await?;
-    dbg!(record);
+
+    // 3) Insert a new ticket
+    let row: (i64,) = sqlx::query_as("insert into ticket (name) values ($1) returning id")
+        .bind("a new ticket")
+        .fetch_one(&pool)
+        .await?;
+
+    // 4) Select all tickets
+    let rows = sqlx::query("SELECT * FROM ticket").fetch_all(&pool).await?;
+    let str_result = rows
+        .iter()
+        .map(|r| format!("{} - {}", r.get::<i64, _>("id"), r.get::<String, _>("name")))
+        .collect::<Vec<String>>()
+        .join(", ");
+    println!("\n== select tickets with PgRows:\n{}", str_result);
+
+    // 5) Select query with map() (build the Ticket manually)
+    let select_query = sqlx::query("SELECT id, name FROM ticket");
+    let tickets: Vec<Ticket> = select_query
+        .map(|row: PgRow| Ticket {
+            id: row.get("id"),
+            name: row.get("name"),
+        })
+        .fetch_all(&pool)
+        .await?;
+    println!("\n=== select tickets with query.map...:\n{:?}", tickets);
+
+    // 6) Select query_as (using derive FromRow)
+    let select_query = sqlx::query_as::<_, Ticket>("SELECT id, name FROM ticket");
+    let tickets: Vec<Ticket> = select_query.fetch_all(&pool).await?;
+    println!("\n=== select tickets with query.map...: \n{:?}", tickets);
+
     Ok(())
 }
