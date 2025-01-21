1bbf8a3e7e9c082b2b4ac18818cd8a1151a3fdd0
magnus <Magnus.s@eclipso.de>
2025-01-21 22:45:08 +0100

    Creating Database has been optimized so that it generates the Database faster, checking_database has problems with big Databases will be worked on in the future

--- src-tauri/src/creating_database.rs
+++ src-tauri/src/creating_database.rs
@@ -1,12 +1,9 @@
-use std::any::{type_name_of_val};
-use std::fs::File;
 use std::sync::mpsc::channel;
-use rusqlite::{Result};
+use rusqlite::{params, Result};
 use threadpool::ThreadPool;
 use walkdir::{WalkDir};
 use r2d2::{Pool, PooledConnection};
 use r2d2_sqlite::SqliteConnectionManager;
-use rusqlite::ffi::{sqlite3_file, sqlite3_stmt, SQLITE_SELECT};
 
 #[derive(Debug)]
 struct Files {
@@ -17,10 +14,16 @@ struct Files {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+
     println!("Creating database");
     let manager = SqliteConnectionManager::file("files.sqlite3");
     let pool = Pool::new(manager)?;
     let conn = pool.get()?;
+
+    let pragma_check: String = conn.query_row("PRAGMA integrity_check", [], |row| row.get(0))?;
+    if pragma_check != "ok" {
+        println!("Database integrity check failed: {}", pragma_check);
+    }
 
     println!("Creating tables");
     let result = conn.execute(
@@ -34,12 +37,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("SQL Message Creating Table: {}", result);
 
-    let _ = create_database(conn, "./Test_dir", 8)?;
+    let _ = create_database(conn, "/", 8)?;
 
 
     let conn = pool.get()?;
 
-    checking_database(conn);
+    //checking_database(conn);
 
     Ok(())
 }
@@ -51,61 +54,60 @@ fn create_database(
 ) -> Result<(), Box<dyn std::error::Error>> {
 
     println!("Populating Database");
-
+    let mut files_vec: Vec<Files> = Vec::new();
     let pool = ThreadPool::new(n_workers);
     let (tx, rx) = channel(); // Channel for communication between threads
+    let mut conn = conn;
 
     println!("Iterating Entries");
     for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) { //This Line does not Throw an Error if the Path supplied is not valid careful
         let tx= tx.clone();
-        println!("Tx has been cloned");
 
         pool.execute(move || {
             let full_path = entry.path().to_owned();
             tx.send(full_path).unwrap();
-            println!("Sent Path")
         });
     }
     drop(tx);
 
     for received in rx {
-        println!("{}, {:?}", type_name_of_val(&received), received);
-
         let file_name = received.file_name().unwrap_or_default().to_string_lossy().to_string();
         let file_type = received.extension().unwrap_or_default().to_string_lossy().to_string();
         let file_path = received.as_path().to_path_buf().to_str().unwrap().to_string();
 
 
-        println!("Instanciating struct");
         let me = Files {
             id: 0,
             file_name,
             file_path,
             file_type,
         };
-
-        println!("{:?}", me);
-        let exists: bool = conn.query_row(
-            "SELECT COUNT(*) > 0 FROM files WHERE file_name = ?1 AND file_path = ?2",
-            (&me.file_name, &me.file_path),
-            |row| row.get(0),
-        ).unwrap_or(false);
-
-        if !exists {
-            conn.execute(
-                "INSERT INTO files (file_name, file_path, file_type) VALUES (?1, ?2, ?3)",
-                (&me.file_name, &me.file_path, &me.file_type),
-            )?;
-            println!("Inserted: {:?}", me);
-        } else {
-            println!("Record already exists: {:?}", me);
-        }
-
+        files_vec.push(me);
     }
+    println!("Number of files to insert: {}", files_vec.len());
+
+    let tx = conn.transaction()?;
+    {
+        let mut stmt = tx.prepare_cached("INSERT INTO files (file_name, file_path, file_type) VALUES (?, ?, ?)")?;
+        for (index, file) in files_vec.iter().enumerate() {
+            match stmt.execute(params![
+            &file.file_name,
+            &file.file_path,
+            &file.file_type,
+        ]) {
+                Ok(_) => println!("Inserted file {}", index),
+                Err(e) => eprintln!("Error inserting file {}: {:?}", index, e),
+            }
+        }
+    }
+    tx.commit()?;
+
     println!("Done with population");
 
     Ok(())
 }
+
+
 fn get_column_as_vec(conn: &PooledConnection<SqliteConnectionManager>, column_name: &str, table_name: &str) -> Result<Vec<String>> {
     let mut stmt = conn.prepare(&format!("SELECT {} FROM {}", column_name, table_name))?;
 
