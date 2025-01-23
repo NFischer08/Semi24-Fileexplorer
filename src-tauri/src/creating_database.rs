50b07c4a0e3824fa85cd7e10f9fae5f5a20ea945
magnus <Magnus.s@eclipso.de>
2025-01-23 11:02:37 +0100

    Creating Database and Checking Database now have a blacklist functionality for file_types so that systemfiles or other files that change all the time don't cause trouble, general Code cleanup, only tested on Linux, on Windows there will probably still be problems

--- src-tauri/src/creating_database.rs
+++ src-tauri/src/creating_database.rs
@@ -1,4 +1,3 @@
-use std::path::Path;
 use std::sync::mpsc::channel;
 use rusqlite::{params, Result};
 use threadpool::ThreadPool;
@@ -16,19 +15,12 @@ struct Files {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 
-    println!("Creating database");
     let manager = SqliteConnectionManager::file("files.sqlite3");
     let pool = Pool::new(manager)?;
     let conn = pool.get()?;
     let thread_count = 8;
 
-    let pragma_check: String = conn.query_row("PRAGMA integrity_check", [], |row| row.get(0))?;
-    if pragma_check != "ok" {
-        println!("Database integrity check failed: {}", pragma_check);
-    }
-
-    println!("Creating tables");
-    let result = conn.execute(
+    let _result = conn.execute(
         "CREATE TABLE IF NOT EXISTS files (
         id   INTEGER PRIMARY KEY,
         file_name TEXT NOT NULL,
@@ -37,15 +29,23 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     )",
         ())?;
 
-    println!("SQL Message Creating Table: {}", result);
+    //println!("SQL Message Creating Table: {}", _result);
 
-    let _ = create_database(conn, "/", thread_count)?;
+    let system_file_extensions = vec![
+        ".o".to_string(), // Objektdatei mit kompiliertem Code
+        ".so".to_string(), // Gemeinsam genutzte Bibliotheksdatei unter Unix-Systemen
+        ".pem".to_string(), // Textdatei für Zertifikate und kryptografische Schlüssel
+        ".lock".to_string(), // Sperrdatei zur Verhinderung gleichzeitiger Zugriffe3
+        ".so.1".to_string(), // Versionierte gemeinsam genutzte Bibliotheksdatei5
+        "".to_string(), // Keine Dateiendung
+    ];
+
+    let _ = create_database(conn, "/", thread_count, &system_file_extensions)?;
 
 
     let conn = pool.get()?;
 
-    checking_database(conn, thread_count);
-
+    checking_database(conn, thread_count, &system_file_extensions)?;
     Ok(())
 }
 
@@ -53,15 +53,14 @@ fn create_database(
     conn: PooledConnection<SqliteConnectionManager>,
     path: &str, // Path to starting Folder of Indexing
     n_workers: usize,
+    system_file_extensions: &Vec<String>,
 ) -> Result<(), Box<dyn std::error::Error>> {
 
-    println!("Populating Database");
     let mut files_vec: Vec<Files> = Vec::new();
     let pool = ThreadPool::new(n_workers);
     let (tx, rx) = channel(); // Channel for communication between threads
     let mut conn = conn;
 
-    println!("Iterating Entries");
     for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) { //This Line does not Throw an Error if the Path supplied is not valid careful
         let tx= tx.clone();
 
@@ -77,14 +76,16 @@ fn create_database(
         let file_type = received.extension().unwrap_or_default().to_string_lossy().to_string();
         let file_path = received.as_path().to_path_buf().to_str().unwrap().to_string();
 
-
-        let me = Files {
-            id: 0,
-            file_name,
-            file_path,
-            file_type,
-        };
-        files_vec.push(me);
+        // Überprüfen, ob die Dateiendung in der Liste der Systemdateien ist
+        if !system_file_extensions.contains(&file_type) {
+            let me = Files {
+                id: 0,
+                file_name,
+                file_path,
+                file_type,
+            };
+            files_vec.push(me);
+        }
     }
     println!("Number of files to insert: {}", files_vec.len());
 
@@ -95,6 +96,7 @@ fn create_database(
         let rows = stmt.query_map([], |row| {
             Ok((row.get::<_, String>(0)?, row.get::<_, String>(1)?))
         })?;
+
         for row in rows {
             if let Ok((name, path)) = row {
                 existing_files.insert((name, path));
@@ -120,8 +122,6 @@ fn create_database(
     }
     tx.commit()?;
 
-    println!("Done with population");
-
     Ok(())
 }
 
@@ -138,25 +138,53 @@ fn get_column_as_vec(conn: &PooledConnection<SqliteConnectionManager>, column_na
 fn checking_database(
     conn: PooledConnection<SqliteConnectionManager>,
     n_workers: usize,
+    system_file_extensions: &Vec<String>,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    println!("Checking database");
 
-    let file_paths = get_column_as_vec(&conn, "file_path", "files").unwrap();
-    let mut bad_paths: Vec<String> = Vec::new();
+    let mut conn = conn;
 
-    let pool = ThreadPool::new(n_workers);
+    let tx = conn.transaction()?;
+    {
+        let mut stmt = tx.prepare_cached("SELECT file_path FROM files")?;
+        let rows = stmt.query_map([], |row| row.get::<_, String>(0))?;
 
-    for path in file_paths {
-        let bad_paths = bad_paths.clone();
-        pool.execute(move || {
-            if Path::new(&path).exists() {
-                let mut bad_paths = bad_paths;
-                bad_paths.push(path);
-            }
-        });
+        let mut bad_paths = Vec::new();
+
+        let pool = ThreadPool::new(n_workers);
+        let (sender, receiver) = channel();
+
+        for path in rows {
+            let path = path?;
+            let sender = sender.clone();
+            let system_file_extensions = system_file_extensions.clone();
+            pool.execute(move || {
+                if !is_system_file(&path, &system_file_extensions) &&
+                    !std::fs::metadata(&path).is_ok() {
+                    sender.send(path).unwrap();
+                }
+            });
+        }
+
+        drop(sender); // Close the sender
+
+        for path in receiver {
+            bad_paths.push(path);
+        }
+
+        //println!("bad files: {:?}", bad_paths);
+        println!("Number of bad files: {}", bad_paths.len());
+
+        // Optionally, remove bad paths from the database
+        //let mut delete_stmt = tx.prepare_cached("DELETE FROM files WHERE file_path = ?")?;
+        //for path in bad_paths {
+        //    delete_stmt.execute([&path])?;
+        //}
     }
-
-    println!("Number of bad files: {}", bad_paths.len());
+    tx.commit()?;
 
     Ok(())
+}
+
+fn is_system_file(path: &str, system_file_extensions: &[String]) -> bool {
+    system_file_extensions.iter().any(|ext| path.ends_with(ext))
 }
