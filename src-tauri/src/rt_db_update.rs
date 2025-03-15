bbcbf67a35183eaacf2124f1e83338d50ebd8c80
Paulonus28 <paulonus28.yt@gmail.com>
2025-03-16 00:11:19 +0100

    added comments and improved event handling logic

--- src-tauri/src/rt_db_update.rs
+++ src-tauri/src/rt_db_update.rs
@@ -1,50 +1,67 @@
-use notify::event::ModifyKind::Name;
-use notify::EventKind::{Create, Modify, Remove};
-use notify::{self, recommended_watcher, Event, RecursiveMode, Result, Watcher};
-use std::collections::HashSet;
-use std::path::PathBuf;
-use std::sync::mpsc::channel;
+use notify::{ self, recommended_watcher, Event, RecursiveMode, Watcher, EventKind::{ Create, Modify, Remove }, event::ModifyKind::Name };
+use std::{collections::HashSet, path::PathBuf, sync::mpsc::channel};
 
 pub fn start_file_watcher(path: PathBuf, allowed_extensions: HashSet<String>) {
     // Create a channel to receive filesystem events
-    let (tx, rx) = channel::<Result<Event>>();
+    let (tx, rx) = channel::<notify::Result<Event>>();
 
-    // Create a watcher
-    let mut watcher = recommended_watcher(tx).expect("Unable to create file watcher");
+    // Create a watcher and return if an error occurs
+    let mut watcher = match recommended_watcher(tx) {
+        Ok(watcher) => watcher,
+        Err(e) => {
+            println!("Error creating watcher: {:?}", e);
+            return;
+        }
+    };
 
-    // Start watching the specified path
-    watcher
-        .watch(&path, RecursiveMode::Recursive)
-        .expect("Unable to watch file");
+    // Start watching the specified path and return if an error occurs
+    match watcher.watch(&path, RecursiveMode::Recursive) {
+        Ok(_) => {}
+        Err(e) => {
+            println!("Error watching path: {:?}", e);
+            return;
+        }
+    }
 
     println!("Watching for changes in {:?}", path);
+    let mut mode: bool = true; // later used to determine if the file needs to be inserted or deleted when modified
 
-    // Spawn a new thread to handle events
+    // Loop to receive events from the channel
     for res in rx {
         match res {
             Ok(event) => {
-                // Check if the event's paths contain any of the allowed extensions
+                // usually only one path is returned (for-loop for safety)
                 for file_path in &event.paths {
-                    if let Some(extension) = file_path.extension() {
-                        if allowed_extensions.contains(&extension.to_string_lossy().to_string()) {
-                            match event.kind {
-                                Create(_) => {
-                                    println!("INSERT {:?}", file_path);
-                                },
-                                Remove(_) => {
-                                    println!("DELETE {:?}", file_path);
-                                },
-                                Modify(modify) => match modify {
-                                    Name(_) => {
-                                        // TODO: Implement file modification handling
-                                        // => 2 MODIFY events are triggered for each file modification
-                                        // => First Path needs to be removed from the database, second Path needs to be added
-                                        println!("Modify {:?}", file_path);
-                                    },
-                                    _ => {}
+                    // check if the path is from interest
+                    if file_path.is_dir() || // TODO: Directories cause problems => scuffed when renaming, etc. => other handling needed?
+                        file_path.extension() // unpack extension and check if it is in the allowed extensions
+                            .map(|ext| allowed_extensions.contains(&ext.to_string_lossy().to_string()))
+                            .unwrap_or(false) {
+                        // get actual event kind and handle it
+                        match event.kind {
+                            Create(_) => {
+                                println!("INSERT {:?}", file_path);
+                            },
+                            Remove(_) => {
+                                println!("DELETE {:?}", file_path);
+                            },
+                            Modify(modify) => match modify {
+                                Name(_) => {
+                                    // TODO: Implement file modification handling
+                                    // => 2 MODIFY events are triggered for each file modification
+                                    // => First Path needs to be removed from the database, second Path needs to be added
+                                    println!("Modify {:?}", file_path);
+                                    // sol attempt !!DOES NOT WORK for folders!!
+                                    if mode {
+                                        println!("DELETE {:?}", file_path);
+                                    } else {
+                                        println!("INSERT {:?}", file_path);
+                                    }
+                                    mode = !mode;
                                 },
                                 _ => {}
-                            }
+                            },
+                            _ => {}
                         }
                     }
                 }
