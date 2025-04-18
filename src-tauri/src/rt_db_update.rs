3bb83f6a914feefe1e4ccada36f1db0c2ae36d15
Paulonus28 <paulonus28.yt@gmail.com>
2025-04-18 18:44:51 +0200

    works except for db connection and folder deletion (seems not fixable)

--- src-tauri/src/rt_db_update.rs
+++ src-tauri/src/rt_db_update.rs
@@ -8,9 +8,9 @@ use notify::{
     EventKind::{Create, Modify, Remove},
     RecursiveMode, Watcher,
 };
-use std::{collections::HashSet, path::PathBuf, sync::mpsc::channel};
+use std::{collections::HashSet, path::{Path, PathBuf}, sync::mpsc::channel, fs};
 
-pub fn start_file_watcher(path: PathBuf, allowed_extensions: HashSet<String>) {
+pub fn start_file_watcher(watch_path: PathBuf, allowed_extensions: HashSet<String>) {
     // Create a channel to receive filesystem events
     let (tx, rx) = channel::<notify::Result<Event>>();
 
@@ -24,7 +24,7 @@ pub fn start_file_watcher(path: PathBuf, allowed_extensions: HashSet<String>) {
     };
 
     // Start watching the specified path and return if an error occurs
-    match watcher.watch(&path, RecursiveMode::Recursive) {
+    match watcher.watch(&watch_path, RecursiveMode::Recursive) {
         Ok(_) => {}
         Err(e) => {
             println!("Error watching path: {:?}", e);
@@ -32,7 +32,18 @@ pub fn start_file_watcher(path: PathBuf, allowed_extensions: HashSet<String>) {
         }
     }
 
-    println!("Watching for changes in {:?}", path);
+    // creates a HashSet with paths to ignore
+    let mut ignore: HashSet<&str> = HashSet::new();
+    let mut trigger: bool = false;
+    #[cfg(target_os = "windows")]
+    {
+        ignore.insert("$Recycle.Bin");
+        ignore.insert("AppData");
+        ignore.insert("Windows\\System32");
+    }
+
+
+    println!("Watching for changes in {:?}", watch_path);
     // Loop to receive events from the channel
     for res in rx {
         match res {
@@ -41,10 +52,20 @@ pub fn start_file_watcher(path: PathBuf, allowed_extensions: HashSet<String>) {
                 for file_path in &event.paths {
                     #[cfg(target_os = "windows")] // for windows: ignore recylce bin
                     {
-                        if file_path.to_string_lossy().contains("C:\\$Recycle.Bin") {
-                            continue;
+                        for folder in &ignore {
+                            if file_path.to_string_lossy().contains(folder) {
+                                trigger = true;
+                                break;
+                            }
                         }
                     }
+
+                    // skip if path which needs to be ignored is found
+                    if trigger {
+                        trigger = false;
+                        continue;
+                    }
+
                     // check if the path is from interest
                     if file_path.is_dir() ||
                     file_path
@@ -55,19 +76,31 @@ pub fn start_file_watcher(path: PathBuf, allowed_extensions: HashSet<String>) {
                         // get actual event kind and handle it
                         match event.kind {
                             Create(_) => {
-                                println!("INSERT {:?}", file_path);
+                                println!("INSERT {:?}", file_path); // TODO: if folder: check content recursive (content doesn't get flagged else)
+                                if file_path.is_dir() {
+                                    todo!()
+                                    // update db function starting at `file_path`
+                                }
                             }
                             Remove(_) => {
-                                println!("DELETE {:?}", file_path);
+                                println!("DELETE {:?}", file_path); // doesn't track folders for whatever reason :(
                             }
                             Modify(modify) => match modify {
                                 // only renaming is interesting
                                 Name(mode) => {
                                     // From gives old path, To gives new path
                                     match mode {
-                                        From => println!("DELETE {:?}", file_path),
-                                        To => println!("INSERT {:?}", file_path),
-                                        _ => {} // proper implementing need if it isnt a normal case
+                                        From => println!("N DELETE {:?}", file_path),
+                                        To => {
+                                            if file_path.is_dir() {
+                                                let ppath = file_path.parent().unwrap_or(Path::new("/")).to_path_buf();
+                                                check_folder(ppath).unwrap_or_default()
+                                            } else {
+                                                println!("N INSERT {:?}", file_path);
+                                            }
+                                        },
+                                        // Linux: `Both` why? Idk, what it means? Idk :(
+                                        _ => println!("Something else {:?}, ({:?})", file_path, mode) // proper implementing need if it isnt a normal case
                                     }
                                 }
                                 _ => {}
@@ -81,4 +114,54 @@ pub fn start_file_watcher(path: PathBuf, allowed_extensions: HashSet<String>) {
         }
     }
     println!("File watcher stopped");
+}
+
+fn get_folders_in_dir(parent_path: PathBuf) -> Result<HashSet<PathBuf>, ()> {
+    let mut folders: HashSet<PathBuf> = HashSet::new();
+    let entries = match fs::read_dir(parent_path) {
+        Ok(entries) => entries,
+        Err(_) => return Err(()),
+    };
+
+    for entry in entries {
+        let entry = match entry {
+            Ok(entry) => entry,
+            Err(_) => continue,
+        };
+        let path = entry.path();
+        if path.is_dir() {
+            folders.insert(path);
+        }
+    }
+    Ok(folders)
+}
+
+fn compare_directories(dir1: &mut HashSet<String>, dir2: &mut HashSet<String>) {
+    let common_el: HashSet<String> = dir1.intersection(dir2).cloned().collect();
+    for el in common_el {
+        dir1.remove(&el);
+        dir2.remove(&el);
+    }
+}
+
+fn check_folder(path: PathBuf) -> Result<(), ()> {
+    let mut current_files: HashSet<String> = match get_folders_in_dir(path) {
+        Ok(paths) => paths.into_iter().map(|path| path.to_string_lossy().to_string()).collect(), // converts HashSet with PathBufs to HashSet with &str (where the paths only point to directories: filter(|path| path.is_dir()).)
+        Err(_) => return Err(()),
+    };
+    let mut db_files: HashSet<String> = HashSet::new(); // TODO: get all files and folders in dir
+    let common_el: HashSet<String> = current_files.intersection(&db_files).cloned().collect();
+    for el in common_el {
+        current_files.remove(&el);
+        db_files.remove(&el);
+    }
+
+    for file in current_files {
+        println!("C INSERT {:?}", file);
+    }
+    for file in db_files {
+        println!("C DELETE {:?}", file);
+    }
+
+    Ok(())
 }
